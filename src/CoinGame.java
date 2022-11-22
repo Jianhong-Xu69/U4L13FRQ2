7361c68d8ded395c0de615377363b2fc3fd8d7e1
jianx <jxu5406@bths.edu>
2022-11-22 18:16:52 -0500

    Fixed thingy

--- src/CoinGame.java
+++ src/CoinGame.java
@@ -1,4 +1,31 @@
 public class CoinGame {
+
+    public static void main(String[] args) {
+        // COPY/PASTE YOUR METHODS for (a) and (b)
+        // into the CoinGame class, then run this code
+        // to test your solutions
+        CoinGame game1 = new CoinGame(10, 5);
+        game1.playGame();  // should print: tie game
+
+        CoinGame game2 = new CoinGame(10, 4);
+        game2.playGame(); // should print: tie game
+
+        CoinGame game3 = new CoinGame(10, 3);
+        game3.playGame(); // should print: player 1 wins
+
+        CoinGame game4 = new CoinGame(10, 2);
+        game4.playGame(); // should print: player 2 wins
+
+        CoinGame game5 = new CoinGame(10, 6);
+        game5.playGame(); // should print: player 2 wins
+
+        CoinGame game6 = new CoinGame(5, 4);
+        game6.playGame(); // should print: player 2 wins
+
+        CoinGame game7 = new CoinGame(7, 4);
+        game7.playGame(); // should print: tie game
+    }
+
     private int startingCoins; // starting number of coins
     private int maxRounds; // maximum number of rounds played
     private int roundTracker = 1; // *** ADDED TO ENABLE TESTING ***
@@ -43,28 +70,35 @@ public class CoinGame {
     }
 
 
-    /**
-     * Plays a simulated game between two players,
-     * as described in part (b).
+    /** Plays a simulated game between two players,
+     as described in part (b).
      */
     public void playGame() {
         int player1Coins = startingCoins;
         int player2Coins = startingCoins;
         int player1Move = -0;
         int player2Move = -0;
-        for (int i = 1; i <= maxRounds; i++) {
-            while (player1Coins > 3 || player2Coins > 3) {
-                player1Move = getPlayer1Move();
-                player2Move = getPlayer2Move(i);
-                player1Coins -= player1Move;
-                player2Coins -= player2Move;
-                if (player1Move == player2Move || player1Move + 1 == player2Move || player1Move - 1 == player2Move) {
-                    player2Coins++;
-                }
-                if (player1Move + 2 == player2Move || player1Move - 2 == player2Move) {
-                    player1Coins += 2;
-                }
+        for (int i = 1; i <= maxRounds; i++){
+            player1Move = getPlayer1Move();
+            player2Move = getPlayer2Move(i);
+            player1Coins -= player1Move;
+            player2Coins -= player2Move;
+            if (player1Move == player2Move || player1Move + 1 == player2Move || player1Move -1 == player2Move){
+                player2Coins++;
             }
+            if (player1Move + 2 == player2Move || player1Move - 2 == player2Move){
+                player1Coins += 2;
+            }
+            if (player1Coins < 3 || player2Coins < 2){
+                i = i + maxRounds;
+            }
+        }
+        if (player1Coins > player2Coins){
+            System.out.println("Player1 Wins");
+        } else if (player1Coins < player2Coins){
+            System.out.println("Player2 Wins");
+        } else {
+            System.out.println("Tie Game");
         }
     }
 }
